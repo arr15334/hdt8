5fcfc8c9e3f10afdf65d69ea1800ca86806a0efc
joiceann <mirandandrea19@gmail.com>
2016-10-01 17:00:47 -0600

    Add files via upload
    Pruebas unitarias

---
+++ Hoja8/VectorHeapTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JoiceAndrea
+ */
+public class VectorHeapTest {
+    
+    public VectorHeapTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    
+    //Se crea objeto del tipo main para pruebas
+    @Test
+    public void testLlenarFicha() {
+        Main.llenarFicha();
+    }
+    
+    @Test
+    public void testMostrarFicha() {
+        Main.mostrarFicha();
+    }
+    
+}
